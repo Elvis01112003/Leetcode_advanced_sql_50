5bd980a571c5268627586fd67409e355e1a44c17
Elvis Titus <120038575+Elvis01112003@users.noreply.github.com>
2026-09-09 10:21:00 +0530

    Create solution.sql

---
+++ 0613-shortest-distance-in-a-line/solution.sql
@@ -0,0 +1,9 @@
+with firstt as (
+                select p1.x as p1x, p2.x as p2x,
+                        abs(p1.x-p2.x) as shortest
+                from Point p1
+                cross join Point p2)
+
+select distinct min(shortest) as shortest
+from firstt
+where shortest >0
